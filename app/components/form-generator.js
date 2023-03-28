460c191bb9609ace6fa0b8e6d37d7916c58a821c
schnoddelbotz <jan@hacker.ch>
2023-03-28 19:27:43 +0200

    Add copy-to clipboard via ember-cli-clipboard
    https://www.npmjs.com/package/ember-cli-clipboard

--- app/components/form-generator.js
+++ app/components/form-generator.js
@@ -4,7 +4,6 @@ import { tracked } from '@glimmer/tracking';
 
 
 export default class FormGeneratorComponent extends Component {
-    @tracked count = 1; 
     // https://guides.emberjs.com/v4.11.0/components/component-state-and-actions/
     // Instead of allowing the component itself to be responsible for the multiple, let's allow it to be passed in.
     // We refer to a component's argument from JavaScript by prefixing them with this.args..
